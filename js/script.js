3997378100ec1a2c11f7e6750efdccc7364e2677
Denzel Sapatin <dknsapatin@gmail.com>
2021-12-26 00:01:28 -0500

    Added Smooth Scrolling & Navigation close menu

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ allLinks.forEach(function (link) {
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
-    // Close mobile naviagtion
+    // Close mobile navigation
     if (link.classList.contains("main-nav-link"))
       headerEl.classList.toggle("nav-open");
   });
